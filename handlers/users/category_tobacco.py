7e45996b3740c4d21e0209be5c5e2a3798cea5ea
Asylbek Abdyrakmanov <asylbekmg@gmail.com>
2020-12-16 01:07:29 +0600

    Created a category tobacco

---
+++ handlers/users/category_tobacco.py
@@ -0,0 +1,12 @@
+from aiogram import types
+
+from loader import dp
+
+
+@dp.message_handler(commands=['tobacco'])
+async def main_questions(message: types.Message):
+    await message.reply(
+        'Маркировка табака\n'
+        '/about_marking - О маркировке\n'
+        '/liability_and_fines - Ответственность участников оборота и штрафы\n'
+    )

06c336f200dba05e276cbe7eccc9184293ed5ae5
stillnurs <noorsultan.mamataliev@gmail.com>
2020-12-24 00:32:47 +0600

    added inline keyboards to: "photo, tires, industrial" categories

--- handlers/users/tires_questions.py
+++ handlers/users/tires_questions.py
@@ -1,126 +1,140 @@
-from aiogram import types
+from aiogram.types import CallbackQuery
 
+from keyboards.inline.category_tires_buttons import category_tires_choice
 from loader import dp
 from data.question_parser import get_headers, get_paragraphs
 
 
-@dp.message_handler(commands=['about_tires'])
-async def about_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='about_tires')
+async def about_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[35])}\n\n'
-        f'{str(get_paragraphs()[34])}'
+        f'{str(get_paragraphs()[34])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['tires_exp'])
-async def tires_exp(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='tires_exp')
+async def tires_exp(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[36])}\n\n'
-        f'{str(get_paragraphs()[35])}'
+        f'{str(get_paragraphs()[35])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['registration_tires'])
-async def registration_tires(message: types.Message):
-    await message.reply(
+@dp.message_handler(text='registration_tires')
+async def registration_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[37])}\n\n'
-        f'{str(get_paragraphs()[36])}'
+        f'{str(get_paragraphs()[36])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['description_tires'])
-async def description_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='description_tires')
+async def description_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[38])}\n\n'
-        f'{str(get_paragraphs()[37])}'
+        f'{str(get_paragraphs()[37])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['payment_tires'])
-async def payment_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='payment_tires')
+async def payment_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[39])}\n\n'
-        f'{str(get_paragraphs()[38])}'
+        f'{str(get_paragraphs()[38])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['ordering_tires'])
-async def ordering_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='ordering_tires')
+async def ordering_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[40])}\n\n'
-        f'{str(get_paragraphs()[39])}'
+        f'{str(get_paragraphs()[39])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['turnover_tires'])
-async def turnover_tires(message: types.Message):
-    await message.reply(
+@dp.message_handler(text='turnover_tires')
+async def turnover_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[41])}\n\n'
-        f'{str(get_paragraphs()[40])}'
+        f'{str(get_paragraphs()[40])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['ede_tires'])
-async def ede_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='ede_tires')
+async def ede_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[42])}\n\n'
-        f'{str(get_paragraphs()[41])}'
+        f'{str(get_paragraphs()[41])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['withdrawal_tires'])
-async def withdrawal_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='withdrawal_tires')
+async def withdrawal_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[43])}\n\n'
-        f'{str(get_paragraphs()[42])}'
+        f'{str(get_paragraphs()[42])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['return_tires'])
-async def return_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='return_tires')
+async def return_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[45])}\n\n'
-        f'{str(get_paragraphs()[44])}'
+        f'{str(get_paragraphs()[44])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['remarking_tires'])
-async def remarking_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='remarking_tires')
+async def remarking_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[46])}\n\n'
-        f'{str(get_paragraphs()[45])}'
+        f'{str(get_paragraphs()[45])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['aggregation_tires'])
-async def aggregation_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='aggregation_tires')
+async def aggregation_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[47])}\n\n'
-        f'{str(get_paragraphs()[46])}'
+        f'{str(get_paragraphs()[46])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['documentation_tires'])
-async def documentation_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='documentation_tires')
+async def documentation_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[48])}\n\n'
-        f'{str(get_paragraphs()[47])}'
+        f'{str(get_paragraphs()[47])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['wholesale_tires'])
-async def wholesale_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='wholesale_tires')
+async def wholesale_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[49])}\n\n'
-        f'{str(get_paragraphs()[48])}'
+        f'{str(get_paragraphs()[48])}',
+        reply_markup=category_tires_choice
     )
 
 
-@dp.message_handler(commands=['import_tires'])
-async def import_tires(message: types.Message):
-    await message.reply(
+@dp.callback_query_handler(text='import_tires')
+async def import_tires(call: CallbackQuery):
+    await call.message.answer(
         f'{str(get_headers()[50])}\n\n'
-        f'{str(get_paragraphs()[49])}'
+        f'{str(get_paragraphs()[49])}',
+        reply_markup=category_tires_choice
     )
-
-
---
+++ keyboards/inline/category_tires_buttons.py
@@ -0,0 +1,66 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+category_tires_choice = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Общие вопросы",
+                             callback_data="about_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Технологии маркировки",
+                             callback_data="labeling_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Регистрация участника",
+                             callback_data="registration_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Описание товара",
+                             callback_data="description_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Оплата кодов",
+                             callback_data="payment_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Заказ кодов",
+                             callback_data="ordering_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Ввод в оборот",
+                             callback_data="turnover_tires")
+    ],
+    [
+        InlineKeyboardButton(text="ЭДО",
+                             callback_data="ede_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Вывод из оборота",
+                             callback_data="withdrawal_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Возврат продукции",
+                             callback_data="return_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Перемаркировка",
+                             callback_data="remarking_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Агрегирование",
+                             callback_data="aggregation_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Документация",
+                             callback_data="documentation_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Оптово-розничное звено",
+                             callback_data="wholesale_tires")
+    ],
+    [
+        InlineKeyboardButton(text="Импорт",
+                             callback_data="import_tires")
+    ],
+
+]
+)

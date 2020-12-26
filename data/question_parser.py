28ac918875939905365fdae174d1aabde29d589a
Scareface <tektonikboy98@gmail.com>
2020-12-26 14:15:36 +0600

    Write to csv

--- data/question_parser.py
+++ data/question_parser.py
@@ -1,17 +1,16 @@
 import requests
+import pandas as pd
+
 from bs4 import BeautifulSoup
 
 
 def get_html():
     """Getting html page content"""
-
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     url = 'https://честныйзнак.рф/vopros-otvet/'
     session = requests.Session()
-    response = session.get(url, headers=headers)
+    response = session.get(url)
     page = response.text
     soup = BeautifulSoup(page, 'html.parser')
-
     return soup
 
 
@@ -22,7 +21,6 @@ def get_headers():
     for i in soup.find_all('i'):
         header = str(i.text)
         titles.append(header.strip())
-
     return titles
 
 
@@ -31,6 +29,25 @@ def get_paragraphs():
     soup = get_html()
     paragraphs = []
     for i in soup.findAll('div', {'class': 'faq-list1__hide'}):
-        p = str(i.text)
-        paragraphs.append(p.strip())
+        p = str(i.get_text().strip())
+        paragraphs.append(p)
     return paragraphs
+
+
+def dataframe():
+    headers = get_headers()
+    paragraphs = get_paragraphs()
+    data = {'headers': headers, 'paragraphs': paragraphs}
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df = df.replace(r'\n', ' ', regex=True)
+    df = df.replace(r'\r', ' ', regex=True)
+    df = df.replace(r'\t', ' ', regex=True)
+    df = df.replace(r'\\t', ' ', regex=True)
+    df = df.replace(r'       ', ' ', regex=True)
+    df = df.replace(r'      ', ' ', regex=True)
+
+    return df.to_csv('text.csv', index=False)
+
+
+text = dataframe()
+print(text)

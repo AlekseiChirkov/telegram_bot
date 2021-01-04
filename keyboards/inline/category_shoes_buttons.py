86a9b6345929f4f10f65df62a9e1f40e5134c44d
Asylbek Abdyrakmanov <asylbekmg@gmail.com>
2021-01-04 12:16:49 +0600

    add a new bk button

--- keyboards/inline/category_shoes_buttons.py
+++ keyboards/inline/category_shoes_buttons.py
@@ -5,5 +5,8 @@ category_shoes_choice = InlineKeyboardMarkup(inline_keyboard=[
         InlineKeyboardButton(text="Вопросы", callback_data="about_shoes"),
 
     ],
+    [
+        InlineKeyboardButton(text="Назад", callback_data="back_main_questions")
+    ],
 ])
 #1
